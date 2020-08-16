d5663c2c818b0129f49b736a13d53be98637cdd5
Sebastian298 <jaimetenorio_geografia@outlook.es>
2020-08-15 21:46:03 -0500

    listado de articulos funcional

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
     
-    #metodo para que se muestren los articulos segun lo requerido
+    #metodo para que se muestre un articulo segun el id que le mande el usuario
     def show
         @article = Article.find(
         params[:id]
         )
+    end
+
+    def index
+        @articles = Article.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show,:index] #cuando haga una peticion el cliente dependiendo de que metodo ira al controlador y buscara index o show
 end
