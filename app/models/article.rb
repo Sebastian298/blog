29e7547eb3c0c4c187d6ef178aecad7f00199b29
Sebastian298 <jaimetenorio_geografia@outlook.es>
2020-08-15 19:47:18 -0500

    se agregaron validaciones en el modelo article

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    validates :tittle, presence: true,length: {minimum:5, maximum:15}     #constrains que ayudan que no se deje en blanco el campo title y description, y especifican una longitud minima en caracteres
+    validates :description, presence: true,length: {minimum:10, maximum:20}
+end
