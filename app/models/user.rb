3a53129a8f999831e94b06f56520e8bcb2da64d4
Sebastian298 <jaimetenorio_geografia@outlook.es>
2020-08-19 16:20:01 -0500

    Validaciones de usuario agregadas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-    
+    #Validaciones para los campos de la tabla usuario, donde se valida que no se deje en blanco el campo
+    #que valide repetidos tomando en cuenta mayuscula y minuscula y la longitud de caracteres
+    validates :username, presence: true,
+                                       uniqueness: { case_sensitive: false }, 
+                                       length: {minimum:3, maximum:30}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i #expr regular para validar el formato de entrada del email
+    validates :email, presence: true, 
+                                       uniqueness: { case_sensitive: false },  #validacion para que tome en cuenta mayusculas y minusculas en el tema de los duplicados
+                                       length: {maximum:110},
+                                       format: { with: VALID_EMAIL_REGEX }
 end
